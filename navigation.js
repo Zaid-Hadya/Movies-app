ba9e77e3d1e97466bc832214574ec1296aed1ec6
Zaid <zaid.omar962@gmail.com>
2024-05-13 18:24:44 +0400

    Change the color to black

--- navigation.js
+++ navigation.js
@@ -3,11 +3,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 import {View, Text} from 'react-native'
 import React from 'react'
+import HomeScreen from './screens/HomeScreen';
+
 export default function Navigation(){
     return (
 <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Navigator screenOptions={{ 
+        headerStyle: {
+          backgroundColor: '#000000', // Black color
+        },
+        headerTintColor: '#ffffff', // Text color
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+        <Stack.Screen name="Movies" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
