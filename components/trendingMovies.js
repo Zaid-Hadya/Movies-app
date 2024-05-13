f30ff5c1db5ea7934d19b8f7bd15a2d20ba46559
Zaid <zaid.omar962@gmail.com>
2024-05-13 21:37:26 +0400

    Add a component

---
+++ components/trendingMovies.js
@@ -0,0 +1,28 @@
+import {View, Text, Touchable, TouchableWithoutFeedback} from 'react-native'
+import React from 'react'
+import Carousel from 'react-native-snap-carousel'
+
+export default function TrendingMovies({data}){
+    return (
+    <View className="mb-8">
+ <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
+ <Carousel
+ data={data}
+ renderItem={({item})=> <MovieCard item ={item} />}
+ firstItem = {1}
+ inactiveSlideOpacity={0.60}
+ sliderWidth={600}
+ itemWidth={400}
+ slideStyle={{display: 'flex', alignItems: 'center'}}
+ />
+  </View>
+    )
+}
+
+const MovieCard = ({item}) => {
+  return (
+    <TouchableWithoutFeedback>
+      <Text className="text-white">Movie</Text>
+    </TouchableWithoutFeedback>
+  )
+}
