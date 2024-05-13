4d7c4bf11d7e596899f06d0382a085e1f9489b90
Zaid <zaid.omar962@gmail.com>
2024-05-13 20:26:09 +0400

    Update the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import {View, Text} from 'react-native'
+import {View, Text, Touchable, TouchableOpacity, ScrollView} from 'react-native'
 import React from 'react'
+import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline';
 
 export default function HomeScreen(){
+const [trending, setTrending] = useState([1,2,3])/*Array def*/
+
     return (
     <View className="flex-1 bg-neutral-800">
-  <Text>HomeScreen</Text>
+  {/*search bar and logo*/}
+  
+   
+  <View className="flex-row justify-between items-center mx-4">
+      <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+      <Text style={{ color: 'white', fontSize: 30, fontWeight: 'bold' }}>
+        <Text style={{ color: 'red' }}>Mov</Text>ies
+      </Text>
+      <TouchableOpacity>
+        <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+      </TouchableOpacity>
     </View>
+  
+      <ScrollView showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom: 10}}>
+        {/*Trending Movies*/}
+      <Trending data={trending} />
+      </ScrollView>
 
+    </View>
     )
 }
