1884b3dac1ce02589b5bebc0234f13c66ca16b59
Zaid <zaid.omar962@gmail.com>
2024-05-14 22:27:59 +0400

    Change the navigation

--- navigation.js
+++ navigation.js
@@ -1,27 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const Stack = createNativeStackNavigator();
-import {View, Text} from 'react-native'
-import React from 'react'
-import HomeScreen from './screens/HomeScreen';
+import { View, Text } from "react-native";
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
 
-export default function Navigation(){
-    return (
-<NavigationContainer>
-<Stack.Navigator screenOptions={{ 
-        headerStyle: {
-          backgroundColor: '#000000', // Black color
-        },
-        headerTintColor: '#ffffff', // Text color
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-        <Stack.Screen name="Movies" component={HomeScreen} />
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#000000", // Black color
+          },
+          headerTintColor: "#ffffff", // Text color
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
-
-
-
-    )
+  );
 }
