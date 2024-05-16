af5dd66e786318ae15490847f3e23f1bbac016e4
Zaid <zaid.omar962@gmail.com>
2024-05-16 17:48:07 +0400

    Fetching data and deleting the array

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -7,6 +7,7 @@ import {
   Modal,
   Button,
   StyleSheet,
+  Image,
 } from "react-native";
 import {
   Bars3CenterLeftIcon,
@@ -17,73 +18,25 @@ import MovieItem from "../components/movieItem";
 const HomeScreen = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [movies, setMovies] = useState([]);
 
-  // Define your data here
-  const data = [
-    {
-      id: 1,
-      title: "Oppenheimer",
-      imageURL:
-        "https://creativereview.imgix.net/content/uploads/2023/12/Oppenheimer.jpg?auto=compress,format&q=60&w=1263&h=2000",
-      description:
-        "Oppenheimer is a 2023 epic biographical thriller drama film written, directed, and produced by Christopher Nolan. It follows the life of J. Robert Oppenheimer, the American theoretical physicist who helped develop the first nuclear weapons during World War II.",
-    },
-    {
-      id: 2,
-      title: "Godzilla x Kong",
-      imageURL:
-        "https://lhsmagpie.com/wp-content/uploads/2024/04/GxK-website-wide.jpg",
-      description:
-        "A giant creature named Kong is on a quest to find his home. However, he and the inhabitants of the Earth face a major obstacle when another titan, whose ambition is to destroy him and the humanity, enters his path. In the first part of the story, Kong fights against Godzilla to prevent him from destroying the planet.",
-    },
-    {
-      id: 3,
-      title: "The Amazing Spider-Man",
-      imageURL:
-        "https://artofthemovies.co.uk/cdn/shop/products/IMG_9256.jpg?v=1662130213",
-      description:
-        "Confident in his powers as Spider-Man, Peter Parker (Andrew Garfield) embraces his new role as a hero and spends time with Gwen Stacy (Emma Stone) in between protecting New York from criminals. However, his greatest battle yet is about to begin. With the emergence of Electro (Jamie Foxx), Peter must confront an enemy far more powerful than he is.",
-    },
-    {
-      id: 4,
-      title: "Joker",
-      imageURL: "https://i.redd.it/3k0ldk1dycl31.jpg",
-      description:
-        "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure. In addition to being a clown at work, Arthur Fleck wants to perform stand-up comedy.",
-    },
-    {
-      id: 5,
-      title: "Pirates of the Caribbean",
-      imageURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwNAJ8rjiQmcbkyUWL4YRBNSX1TMjaartc4luB0x3Zb2Cg1vhpfvMNxNylsrYlY0wu6kA&usqp=CAU",
-      description:
-        "The film stars Johnny Depp, Geoffrey Rush, Orlando Bloom, and Keira Knightley. The story follows pirate Captain Jack Sparrow (Depp) and blacksmith Will Turner (Bloom) as they rescue the kidnapped Elizabeth Swann (Knightley) from the crew of the Black Pearl, captained by Hector Barbossa (Rush).",
-    },
-    {
-      id: 6,
-      title: "Shrek",
-      imageURL:
-        "https://image.tmdb.org/t/p/original/cG3Am50LUD64j8uTyjLFUpve40x.jpg",
-      description:
-        "A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back. It's the story of a terrifying green ogre by the name of Shrek, who lives in a swamp.",
-    },
-    {
-      id: 7,
-      title: "Jaws",
-      imageURL:
-        "https://alltherightmovies.com/wp-content/uploads/2023/04/Capture-4-e1681473230496.jpg",
-      description:
-        "Jaws, American suspense and horror film, released in 1975, that was directed by Steven Spielberg and is considered the first summer blockbuster. Based on the 1974 novel of the same name by author Peter Benchley, it tells the story of a great white shark that terrorizes a beach resort town.",
-    },
-    {
-      id: 8,
-      title: "Avengers Infinity War",
-      imageURL:
-        "https://m.media-amazon.com/images/I/71eHZFw+GlL._AC_UF1000,1000_QL80_.jpg",
-      description:
-        "On a mission to collect all six Infinity Stones, Thanos plans to use the artifacts to inflict his twisted will on reality. The fate of the planet and existence itself has never been more uncertain as everything the Avengers have fought for has led up to this moment.",
-    },
-  ];
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=300d2fb47e3f5f8d5e569ce27884acdc"
+      );
+      const data = await response.json();
+      if (data.results) {
+        setMovies(data.results);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   const handlePress = (movie) => {
     setSelectedMovie(movie);
@@ -104,12 +57,23 @@ const HomeScreen = () => {
 
       <ScrollView>
         {/* Display movie list */}
-        {data.map((item) => (
-          <MovieItem
+        {movies.map((item) => (
+          <TouchableOpacity
             key={item.id}
-            item={item}
+            style={styles.movieItem}
             onPress={() => handlePress(item)}
-          />
+          >
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w342/${item.backdrop_path}`,
+              }}
+              style={{ width: "100%", height: 200 }}
+              resizeMode="contain"
+            />
+            <Text style={{ color: "white" }}>{item.title}</Text>
+            <Text>{item.release_date}</Text>
+            <Text style={{ color: "white" }}>{item.overview}</Text>
+          </TouchableOpacity>
         ))}
       </ScrollView>
 
