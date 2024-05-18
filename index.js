dcfd97bd325cbcd3475efff747661a8bbb60a0b1
Zaid <zaid.omar962@gmail.com>
2024-05-18 15:15:58 +0400

    Create index.js to establish basic HTTP server

---
+++ index.js
@@ -0,0 +1,23 @@
+{
+  /*Setting up a basic HTTP server*/
+}
+
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ foo: "bar" }));
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log("Server running at http://${hostname}:${port}/");
+});
